bd9ce1a5ce912a5338cb68d1f30b7e190fff3af0
Ricardo Tan <ricardo.tan.2000@gmail.com>
2024-07-08 21:39:41 +0700

    change image src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import LogoFull from "../../public/logo-full.png";
+import BookingIllustration from '../../public/booking-illustration.png';
 import BookingIllustrationBg from "../../public/booking-illustration-bg.png";
 
 export default function Home() {
@@ -6,11 +8,11 @@ export default function Home() {
     <main className="w-screen font-nexa">
       <section className="flex flex-col w-full h-full flex-column items-center mt-20">
         <div className="w-1/2 mb-5 lg:w-10">
-          <Image src="/Logo.png" className="h-full w-full" alt="Neema Logo" width={300} height={150} />
+          <Image src={LogoFull.src} className="h-full w-full" alt="Neema Logo" width={300} height={150} />
         </div>
         <p className="text-4xl font-bold text-center my-5">Mau punya aplikasi reservasi pribadi?</p>
         <div className="flex justify-center items-center mt-5 w-full h-72 relative" style={{ backgroundImage: `url(${BookingIllustrationBg.src})` }}>
-          <Image className="w-10/12 h-full object-contain absolute bottom-12 left-0 right-0" src={"/booking-illustration.png"} width={400} height={400} alt={"booking-illustration"} />
+          <Image className="w-10/12 h-full object-contain absolute bottom-12 left-0 right-0" src={BookingIllustration.src} width={400} height={400} alt={"booking-illustration"} />
         </div>
       </section>
     </main>
