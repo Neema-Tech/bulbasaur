91fcc58257a975152e94ba56d3b3d19980253008
Ricardo Tan <ricardo.tan.2000@gmail.com>
2024-07-09 03:24:54 +0700

    added seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,11 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest"></link>
+        
+        <title>Neema - Aplikasi Reservasi Pribadi</title>
+        <meta name="description" content="Neema membantu pemilik bisnis di Indonesia mengelola pemesanan reservasi dengan mudah. Buat situs reservasi Anda sendiri dalam tiga langkah sederhana." />
+        <meta name="keywords" content="Neema, aplikasi reservasi, pemesanan lapangan, bisnis Indonesia, situs reservasi, booking management, booking, aplikasi booking, booking app, booking solution, website booking, website reservasi, bikin website" />
+        <meta name="author" content="Neema Team" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
