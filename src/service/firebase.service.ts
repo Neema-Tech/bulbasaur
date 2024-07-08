beb32d4a639a251459717475f3bc75d21242e82b
Kennard Alcander <kennard@hellonimbly.com>
2024-07-09 02:19:53 +0700

    Feat: Remove console + add validation

--- src/service/firebase.service.ts
+++ src/service/firebase.service.ts
@@ -10,12 +10,14 @@ import {
 
 export const addUserEmail = async (email: string) => {
   try {
-    console.log("HERE");
+    if (!email) return "";
+
     const headerRef = ref(database, `userEmail`); // Get ref of 'data'
     const existingValue = await get(
       query(headerRef, orderByChild("email"), equalTo(email))
     );
     if (existingValue.val()) return "";
+
     const snapshot = await push(headerRef, { email }); // Get data of 'data'
     return snapshot.key;
   } catch (error) {
