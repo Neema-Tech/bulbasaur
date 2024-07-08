cec676587b8f7c1ac06c13ac37594cd33797dbcd
Ricardo Tan <ricardo.tan.2000@gmail.com>
2024-07-08 20:36:36 +0700

    checkpoint landing page not yet done

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import BookingIllustrationBg from "../../public/booking-illustration-bg.png";
+
+export default function Home() {
+  return (
+    <main className="w-screen font-nexa">
+      <section className="flex flex-col w-full h-full flex-column items-center mt-20">
+        <div className="w-1/2 mb-5 lg:w-10">
+          <Image src="/Logo.png" className="h-full w-full" alt="Neema Logo" width={300} height={150} />
+        </div>
+        <p className="text-4xl font-bold text-center my-5">Mau punya aplikasi reservasi pribadi?</p>
+        <div className="flex justify-center items-center mt-5 w-full h-72 relative" style={{ backgroundImage: `url(${BookingIllustrationBg.src})` }}>
+          <Image className="w-10/12 h-full object-contain absolute bottom-12 left-0 right-0" src={"/booking-illustration.png"} width={400} height={400} alt={"booking-illustration"} />
+        </div>
+      </section>
+    </main>
+  );
+}
