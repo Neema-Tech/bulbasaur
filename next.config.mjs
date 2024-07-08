6ce1a557994c36c4459d3de1d62b5ae13c0550fa
Ricardo Tan <ricardo.tan.2000@gmail.com>
2024-07-08 21:10:43 +0700

    Added bulbasaur as base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "",
+  basePath: "/bulbasaur",
   output: "export",
   reactStrictMode: true,
   images: { unoptimized: true }
