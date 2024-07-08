15411db205e24930535675e696faa6370e62a699
Ricardo Tan <ricardo.tan.2000@gmail.com>
2024-07-09 00:15:32 +0700

    Added new form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,14 @@ export default function Home() {
   return (
     <main className="w-screen h-screen font-nexa">
       <section className="flex flex-col w-full h-full flex-column items-center pt-10">
-        <div className="w-1/2 mb-5 lg:w-10">
+        <div className="w-[200px] mb-5 md:w-[300px]">
           <Image src={LogoFull.src} className="h-full w-full" alt="Neema Logo" width={300} height={150} />
         </div>
-        <p className="text-3xl font-bold text-center my-5 mb-8">Mau punya aplikasi reservasi pribadi?</p>
-        <div className="flex justify-center items-end mt-5 w-full h-full relative bg-no-repeat bg-cover bg-center pb-16" style={{ backgroundImage: `url(${BookingIllustrationBg.src})` }}>
-          <Image className="w-10/12 h-full object-contain absolute left-0 right-0" src={BookingIllustration.src} width={400} height={400} alt={"booking-illustration"} style={{ bottom: '120px' }} />
+        <p className="text-3xl font-bold text-center py-5 pb-8 md:text-[70px] md:leading-[80px]">Mau punya aplikasi reservasi pribadi?</p>
+        <div className="flex flex-col justify-center items-center mt-5 w-full h-full bg-no-repeat bg-cover bg-center pb-16" style={{ backgroundImage: `url(${BookingIllustrationBg.src})` }}>
+          <div className="w-full h-full max-h-[280px] relative flex justify-center items-center md:max-h-[500px]">
+            <Image className="w-full h-full object-contain absolute right-4 bottom-11 md:bottom-[80px]" src={BookingIllustration.src} width={400} height={400} alt={"booking-illustration"} />
+          </div>
           <div className="flex flex-col w-full px-6">
             <form action="#" method="post" className="flex justify-between w-full bg-white pl-3 py-2 pr-2 rounded-full overflow-hidden">
               <input type="email" name="email" placeholder="Ketik email anda..." className="flex flex-1 mr-2" />
